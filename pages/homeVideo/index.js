b562bd4308705a89083fd97ab1e06858823374e3
jiangsusu <15798007586@163.com>
2022-02-16 23:12:29 +0800

    下拉刷新

--- pages/homeVideo/index.js
+++ pages/homeVideo/index.js
@@ -1,16 +1,36 @@
 Page({
     data: {
-        topMVs: []
+      topMVs: [],
+      hasMore: true,
     },
     onLoad: function () {
-        wx.request({
-          url: 'http://123.207.32.32:9001/top/mv',
-          method: 'GET',
-          success: (res) => {
-            this.setData({
-                topMVs: res.data.data || []
-            })
-          }
-        })
+      this.loadData()
     },
+    onPullDownRefresh: function() {
+      this.loadData(0)
+    },
+    loadData: function (offset = 0) {
+      wx.request({
+        url: `http://123.207.32.32:9001/top/mv?limit=10&offset=${offset}`,
+        method: 'GET',
+        success: (res) => {
+          let newTopMvs = this.data.topMVs;
+          if (offset === 0) {
+            newTopMvs = res.data.data;
+            wx.stopPullDownRefresh();
+          } else {
+            newTopMvs.push(...res.data.data)
+          }
+          console.log('newTopMvs', newTopMvs)
+          this.setData({
+            topMVs: newTopMvs,
+            hasMore: res.data.hasMore
+          })
+        }
+      })
+    },
+    onReachBottom: function () {
+      if (!this.data.hasMore) return;
+      this.loadData(this.data.topMVs.length)
+    }
 })
